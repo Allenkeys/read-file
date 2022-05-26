c9bf7c4e8706d957c7365e4b76f2a2db6bf6bca4
Joshua <ezejoshuac@gmail.com>
2022-05-26 17:54:42 +0100

    Refactored readFileContent function to remove punctuations

--- main.py
+++ main.py
@@ -3,21 +3,25 @@
 # count_words("The cake is done. It is a big cake!") 
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
-def read_file_content(filename):
-    # [assignment] Add your code here 
+# Create a list of punctuations which should be removed from the content of the file
+regexList = [',', '?', '.', '!']
+
+def readFileContent(filename):
     with open(filename, 'r') as txt:
         content = txt.read()
-        content = content.replace(',', '')
+        for item in regexList:
+            content = content.replace(item, '')
         return content
 
-    
-    # return filename
 
 
-# def count_words():
-#     text = read_file_content("./story.txt")
+# def countWords():
+#     text = readFileContent("./story.txt")
 #     # [assignment] Add your code here
-
+#     text = text.split()
 #     return {"as": 10, "would": 20}
 
-print(read_file_content('./story.txt'))
+
+
+print(readFileContent('./story.txt'))
+
